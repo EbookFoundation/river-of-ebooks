25b191d04cf3c570600c72ff069960c91c0ef770
unknown <villa7.kluge5@gmail.com>
2018-10-28 18:12:33 -0400

    add new packages and finish webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const SassWebpackPlugin = require('sass-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     login: './assets/js/login.js'
   },
   output: {
-    path: __dirname + '/.tmp/public',
+    path: path.join(__dirname, '/.tmp/public'),
     filename: '[name].bundle.js'
   },
   module: {
@@ -23,7 +26,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'assets/templates/login.html'
-    })
+      template: 'assets/templates/login.html',
+      links: [{ rel: 'stylesheet', type: 'text/css', href: 'login.css' }],
+      filename: path.join(__dirname, '/.tmp/public/login.html')
+    }),
+    new SassWebpackPlugin(['assets/styles/login.scss'], process.env.NODE_ENV),
   ]
 };
