af125ba5358f23f626ed52977e8a56aba5986dee
unknown <villa7.kluge5@gmail.com>
2018-10-29 19:56:18 -0400

    add better starts

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 const path = require('path')
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: {
-    login: './assets/js/login.js'
-  },
-  output: {
-    path: path.join(__dirname, '/.tmp/public'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        use: 'babel-loader',
-        test: /\.jsx?$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development'
+
+  return {
+    mode: mode || 'development',
+    entry: {
+      login: './assets/js/login.js'
+    },
+    output: {
+      path: path.join(__dirname, '/.tmp/public'),
+      filename: '[name].bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          use: 'babel-loader',
+          test: /\.jsx?$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'assets/templates/login.html',
+        links: mode === 'production' ? [{ rel: 'stylesheet', type: 'text/css', href: 'login.css' }] : [],
+        filename: path.join(__dirname, '/.tmp/public/login.html')
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'assets/templates/login.html',
-      links: process.env.NODE_ENV === 'production' ? [{ rel: 'stylesheet', type: 'text/css', href: 'login.css' }] : [],
-      filename: path.join(__dirname, '/.tmp/public/login.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  ]
+  }
 }
