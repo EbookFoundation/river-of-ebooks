935058df2800137dd4b3a511e81c9225b557503e
unknown <villa7.kluge5@gmail.com>
2018-11-14 22:51:29 -0500

    add targetcontroller and model

--- api/controllers/TargetController.js
+++ api/controllers/TargetController.js
@@ -1,8 +1,49 @@
+'use strict'
+const HttpError = require('../errors/HttpError')
+
 module.exports = {
   show: function (req, res) {
     res.view('pages/targets', {
       email: req.user.email
 
     })
+  },
+  create: async function (req, res) {
+    try {
+      const url = await TargetUrl.create({
+        user: req.user
+      }).fetch()
+      return res.json(url)
+    } catch (e) {
+      return (new HttpError(500, e.message)).send(res)
+    }
+  },
+  edit: async function (req, res) {
+    try {
+      const id = req.param('id')
+      const value = req.param('url')
+      const url = await TargetUrl.update({ id, user: req.user }, { url: value }).fetch()
+      return res.json(url)
+    } catch (e) {
+      return (new HttpError(500, e.message)).send(res)
+    }
+  },
+  delete: async function (req, res) {
+    try {
+      await TargetUrl.destroy({ id: req.param('id') })
+      return res.status(204)
+    } catch (e) {
+      return (new HttpError(500, e.message)).send(res)
+    }
+  },
+  list: async function (req, res) {
+    try {
+      const urls = await TargetUrl.where({
+        user: req.user
+      })
+      return res.json(urls)
+    } catch (e) {
+      return (new HttpError(500, e.message)).send(res)
+    }
   }
 }
