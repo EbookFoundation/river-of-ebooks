9ddc8ff2716b160908f763e522c03a9c67b619d5
unknown <villa7.kluge5@gmail.com>
2018-11-19 14:57:48 -0500

    add knex migration to create pg tables

---
+++ knexfile.js
@@ -0,0 +1,4 @@
+module.exports = {
+  client: 'pg',
+  connection: 'postgresql://devdbo:password@roe-db-dev.cw5d6ibguemf.us-east-2.rds.amazonaws.com:5432/roedev'
+}
---
+++ migrations/20181119144327_create_initial_tables.js
@@ -0,0 +1,45 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('user', t => {
+      t.increments('id').primary()
+      t.string('email').notNullable()
+      t.timestamps()
+    }),
+    knex.schema.createTable('passport', t => {
+      t.increments('id').primary()
+      t.string('protocol').notNullable()
+      t.string('password')
+      t.string('accesstoken')
+      t.string('provider')
+      t.string('identifier')
+      t.json('tokens')
+      t.integer('user').notNullable().references('user.id')
+      t.timestamps()
+    }),
+    knex.schema.createTable('targeturl', t => {
+      t.increments('id').primary()
+      t.integer('user').notNullable().references('user.id')
+      t.string('url')
+      t.timestamps()
+    }),
+    knex.schema.createTable('book', t => {
+      t.increments('id').primary()
+      t.string('source')
+      t.string('storage')
+      t.string('title').notNullable()
+      t.string('author')
+      t.string('version')
+      t.string('isbn')
+      t.timestamps()
+    })
+  ])
+}
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('book'),
+    knex.schema.dropTable('targeturl'),
+    knex.schema.dropTable('passport'),
+    knex.schema.dropTable('user')
+  ])
+}
