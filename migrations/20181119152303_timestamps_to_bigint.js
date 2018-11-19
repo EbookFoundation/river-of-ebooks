098f8f6b115115ceaf768fd3bd5f3ebb2cec5099
unknown <villa7.kluge5@gmail.com>
2018-11-19 15:27:37 -0500

    knex requires columns be dropped before changing

--- migrations/20181119152303_timestamps_to_bigint.js
+++ migrations/20181119152303_timestamps_to_bigint.js
@@ -1,18 +1,22 @@
 exports.up = function (knex, Promise) {
   return Promise.all([
     knex.schema.table('user', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.bigInteger('created_at')
       t.bigInteger('updated_at')
     }),
     knex.schema.table('passport', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.bigInteger('created_at')
       t.bigInteger('updated_at')
     }),
     knex.schema.table('targeturl', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.bigInteger('created_at')
       t.bigInteger('updated_at')
     }),
     knex.schema.table('book', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.bigInteger('created_at')
       t.bigInteger('updated_at')
     })
@@ -22,18 +26,22 @@ exports.up = function (knex, Promise) {
 exports.down = function (knex, Promise) {
   return Promise.all([
     knex.schema.table('user', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.integer('created_at')
       t.integer('updated_at')
     }),
     knex.schema.table('passport', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.integer('created_at')
       t.integer('updated_at')
     }),
     knex.schema.table('targeturl', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.integer('created_at')
       t.integer('updated_at')
     }),
     knex.schema.table('book', t => {
+      t.dropColumns('created_at', 'updated_at')
       t.integer('created_at')
       t.integer('updated_at')
     })
